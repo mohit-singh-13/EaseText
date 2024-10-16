418ecfcbb960c5cbaa33dfb75728274b36b57dc3
mohit-singh-13 <mohitsingh092003@gmail.com>
2024-10-16 21:03:35 +0530

    Some minor fixed in contact form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,7 +20,7 @@ const Form = () => {
   }
 
   return (
-    <div className="bg-[#36B8B880] rounded-[2.8rem] md:w-[75%] w-[95%] h-[75% h-[85%] flex flex-col px-[2.5rem] py-[2.5rem] gap-6 libre-caslon-text-regular">
+    <div className="bg-[#36B8B880] rounded-[2.8rem] xl:w-[75%] w-[95%] h-[75% h-[85%] flex flex-col px-[2.5rem] py-[2.5rem] gap-4 xl:gap-6 libre-caslon-text-regular">
       <div className="flex flex-col 2xl:flex-row">
         <p className="text-[2rem] 2xl:w-[30%] w-full">Name</p>
         <input required type="text" name="name" value={formData.name} onChange={changeHandler} className="bg-[#099C8A] rounded-[4.4rem] 2xl:w-[70%] w-full px-[1rem] h-[3rem] text-white border" />
@@ -38,7 +38,7 @@ const Form = () => {
 
       <div className="flex h-full flex-col 2xl:flex-row">
         <p className="text-[2rem] 2xl:w-[30%] w-full">Message</p>
-        <textarea required name="message" id="message" value={formData.message} onChange={changeHandler} className="bg-[#099C8A] rounded-[3rem] resize-none 2xl:w-[70%] w-full h-[8rem] text-white px-[1.5rem] py-[0.8rem] border"></textarea>
+        <textarea required name="message" id="message" value={formData.message} onChange={changeHandler} className="bg-[#099C8A] rounded-[3rem] resize-none 2xl:w-[70%] 4xl:h-full w-full h-[8rem] text-white px-[1.5rem] py-[0.8rem] border"></textarea>
       </div>
 
       <button className="bg-[#3b5855] rounded-[4.4rem] self-end mr-[1rem] px-[2.2rem] py-[0.8rem] text-white font-bold uppercase tracking-widest">Send</button>
