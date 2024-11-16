23d45f8afd1c788a2ccf524a1621364019e999b7
mohit-singh-13 <mohitsingh092003@gmail.com>
2024-11-16 14:22:06 +0530

    Update loader

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useCallback, useEffect, useState } from "react"
+import { useState } from "react"
 import Loader from "./Loader";
 
 const HeroSection = () => {
@@ -13,35 +13,35 @@ const HeroSection = () => {
 
   const submitHandler = async () => {
     setLoader(true);
-    const res = await axios.post("http://ec2-54-243-16-141.compute-1.amazonaws.com:8080/simplify", {
+    const res = await axios.post("http://localhost:8080/simplify", {
       data: {
         complex_text: { text }
       }
 
     });
 
-    console.log(res)
-    setResult(res.data.simplified_text)
+    console.log(res);
+    setResult(res.data.simplified_text);
     setLoader(false);
   }
 
   return (
-    loader ? <Loader /> :
-      <div className="w-full h-[105vh] relative">
-        <div className="absolute inset-0 bg-computer bg-center opacity-50 bg-no-repeat  bg-[length:58.5rem_45rem] top-[-5rem]"></div>
+    <div className="w-full h-[105vh] relative ">
+      { loader ? <Loader /> : null }
+      <div className="absolute inset-0 bg-computer bg-center opacity-50 bg-no-repeat  bg-[length:58.5rem_45rem] top-[-5rem]"></div>
 
-        <div className="relative w-[90%] mx-auto pt-[2rem] h-full flex">
-          <div className="flex justify-between items-center  relative h-[85%] w-full 4xl:flex-row flex-col gap-5">
-            <textarea name="input" id="input" placeholder="Type Here..." value={text} onChange={changeHandler} className="text-[1.3rem] w-[90%] xl:w-[36rem] h-[30rem] rounded-[3.4rem] p-[2rem] bg-white resize-none font-[Inter]">
-            </textarea>
+      <div className="relative w-[90%] mx-auto pt-[2rem] h-full flex">
+        <div className="flex justify-between items-center  relative h-[85%] w-full 4xl:flex-row flex-col gap-5">
+          <textarea name="input" id="input" placeholder="Type Here..." value={text} onChange={changeHandler} className="text-[1.3rem] w-[90%] xl:w-[36rem] h-[30rem] rounded-[3.4rem] p-[2rem] bg-white resize-none font-[Inter]">
+          </textarea>
 
-            <textarea name="output" id="output" placeholder="Result..." readOnly value={result} className="text-[1.3rem] w-[90%] xl:w-[36rem] h-[30rem] rounded-[3.4rem] p-[2rem] resize-none font-[Inter]">
-            </textarea>
+          <textarea name="output" id="output" placeholder="Result..." readOnly value={result} className="text-[1.3rem] w-[90%] xl:w-[36rem] h-[30rem] rounded-[3.4rem] p-[2rem] resize-none font-[Inter]">
+          </textarea>
 
-            <button className="absolute left-[50%] translate-x-[-5rem] bottom-[-4rem] 4xl:bottom-[-7%] 5xl:top-[50%] 5xl:bottom-[43%] py-[0.5rem] rounded-[4.4rem] w-[10rem] text-white font-[Inter] border-[2px] border-white cursor-pointer hover:bg-btn hover:text-black hover:font-bold transition-all duration-200" onClick={submitHandler}>Simplify</button>
-          </div>
+          <button className="absolute left-[50%] translate-x-[-5rem] bottom-[-4rem] 4xl:bottom-[-7%] 5xl:top-[50%] 5xl:bottom-[43%] py-[0.5rem] rounded-[4.4rem] w-[10rem] text-white font-[Inter] border-[2px] border-white cursor-pointer hover:bg-btn hover:text-black hover:font-bold transition-all duration-200" onClick={submitHandler}>Simplify</button>
         </div>
       </div>
+    </div>
   )
 }
 
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,8 @@
 const Loader = () => {
   return (
-    <div className="h-[100vh] w-[100vw] flex justify-center items-center">
-      <div className="loader"></div>
+    <div className="h-[120%] translate-y-[-8rem] w-full justify-center items-center flex absolute z-[200]">
+      <div className="absolute bg-[#4e4e4e79] h-full w-screen"></div>
+      <div className="spinner"></div>
     </div>
 
   )
